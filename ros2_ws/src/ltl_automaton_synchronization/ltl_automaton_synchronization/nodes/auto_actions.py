3647cdd4e28b4ab13da6c5845f112195ab182311
DavidePeron19 <davide.peron19@gmail.com>
2024-05-06 13:46:14 +0200

    nothing

--- ros2_ws/src/ltl_automaton_synchronization/ltl_automaton_synchronization/nodes/auto_actions.py
+++ ros2_ws/src/ltl_automaton_synchronization/ltl_automaton_synchronization/nodes/auto_actions.py
@@ -112,8 +112,8 @@ class SynchroActions(Node):
         client_cb_group = MutuallyExclusiveCallbackGroup()
         self.finish_collab_srv = self.create_client(FinishCollab, "finished_collab", callback_group=client_cb_group)
 
-        self.cmd_pub = self.create_publisher(Twist, 'cmd_vel', queue_size=10)
-        #comment
+        self.cmd_pub = self.create_publisher(Twist, 'cmd_vel', 10)
+
         # MOCAP subscribers
         mocap_cb_group = ReentrantCallbackGroup()
         self.obs_sub={}
