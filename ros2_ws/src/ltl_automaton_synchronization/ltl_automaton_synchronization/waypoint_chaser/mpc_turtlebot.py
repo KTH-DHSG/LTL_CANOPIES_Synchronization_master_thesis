05e55410ab7b7d762f0fdcfc46509be3b59fd43e
DavidePeron19 <davide.peron19@gmail.com>
2024-05-24 17:52:45 +0200

    test

--- ros2_ws/src/ltl_automaton_synchronization/ltl_automaton_synchronization/waypoint_chaser/mpc_turtlebot.py
+++ ros2_ws/src/ltl_automaton_synchronization/ltl_automaton_synchronization/waypoint_chaser/mpc_turtlebot.py
@@ -198,6 +198,7 @@ class MPC_Turtlebot():
         # define the barrier function
         print(obs)
         print(type(obs[0]))
+        print(type(X[0]))
         h=ca.sqrt((X[0]-obs[0]/1.)**2+(X[1]-obs[1]/1.)**2)-(self.rob_diam/2.+obs[2]/1.)
         # calcilating the x_dot
         x_dot= self.f(X, U)        
